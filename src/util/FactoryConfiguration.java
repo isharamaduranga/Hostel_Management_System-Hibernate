c446404e2b3eda1d4097c092040585c71d0f9009
Ishara Maduranga <isharamaduranga500@gmail.com>
2022-06-16 20:22:23 +0530

    Created FactoryConfiguration Class With Util file for the configure to Annotated classes into the  whole project.

---
+++ src/util/FactoryConfiguration.java
@@ -0,0 +1,43 @@
+/**
+ * @author : Ishara Maduarnga
+ * Project Name: Hibernate_Cw_Project
+ * Date        : 6/16/2022
+ * Time        : 8:13 PM
+ * Year        : 2022
+ */
+
+package util;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.io.IOException;
+import java.util.Properties;
+
+public class FactoryConfiguration {
+    private static FactoryConfiguration factoryConfiguration;
+
+    private SessionFactory sessionFactory;
+
+    private FactoryConfiguration() throws IOException {
+        Configuration configuration = new Configuration();
+        //configuring properties file
+        Properties p = new Properties();
+        p.load(Thread.currentThread().getContextClassLoader().getResourceAsStream("hibernate.properties"));
+        configuration.setProperties(p);
+
+        //configuring entities
+
+
+        sessionFactory = configuration.buildSessionFactory();
+    }
+
+    public static FactoryConfiguration getInstance() throws IOException {
+        return factoryConfiguration==null? factoryConfiguration = new FactoryConfiguration():factoryConfiguration;
+    }
+
+    public Session getSession(){
+        return sessionFactory.openSession();
+    }
+}
